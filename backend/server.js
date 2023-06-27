9238612e9daf44c79ba58ecce283e2384b2b6e81
Sudesh Nayak <103251710+sudesh003@users.noreply.github.com>
2023-06-27 22:49:12 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const port = 8000;
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+
+const users = {};
+
+io.on('connection', (socket) => {
+    // If any new user joins, let other users connected to the server know!
+    socket.on('new-user-joined', (name) => {
+        users[socket.id] = name;
+        io.emit('users-update', Object.values(users));
+    });
+
+    // If someone sends a message, broadcast it to other people
+    socket.on('send', (message) => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+    });
+
+    // If someone leaves the chat, let others know
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+        io.emit('users-update', Object.values(users));
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ src/MyComponents/home.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect, useRef } from 'react';
+import chatlogo from './cat2.jpg';
+import './homeStyle.css';
+import { FaUsers, FaPaperPlane } from 'react-icons/fa';
+import io from 'socket.io-client';
+const socket = io.connect('http://localhost:8000');
+
+function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const messageBoxRef = useRef(null);
+
+  useEffect(() => {
+    const name = prompt('Enter your anonymous name');
+    socket.emit('new-user-joined', name);
+
+    socket.on('users-update', (updatedUsers) => {
+      setUsers(updatedUsers);
+    });
+
+    socket.on('receive', (data) => {
+      append({ content: data.message, sender: data.name }, 'left');
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    // Scroll to the bottom when messages change
+    if (messageBoxRef.current) {
+      messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  const append = (message, position) => {
+    setMessages((prevMessages) => [...prevMessages, { content: message.content, position: position, sender: message.sender }]);
+  };
+
+  const handleInputChange = (e) => {
+    let text = e.target.innerText;
+    if (e.key === '\n') {
+      text += '\u2003';
+    }
+    setMessage(text);
+  };
+  const handleSend = () => {
+    if (message.trim() !== '') {
+      const newMessage = {
+        content: message,
+        sender: 'You',
+      };
+      append(newMessage, 'right');
+      socket.emit('send', message);
+      setMessage('');
+      const textField = document.querySelector('.textField');
+      textField.textContent = '';
+    }
+  };
+
+  const togglePanel = () => {
+    setIsOpen(!isOpen);
+  };
+
+  function playAudio(){
+    const audioEl = document.getElementsByClassName("audio-element")[0]
+    audioEl.play()
+  };
+
+  return (
+    <>
+      <audio className="audio-element">
+          <source src="https://assets.coderrocketfuel.com/pomodoro-times-up.mp3"></source>
+      </audio>
+      <div className="header">
+        <img className="logo" src={chatlogo} alt="chat symbol"/>
+        <h1>Cat Chat</h1>
+        <button className="panelButton" onClick={togglePanel}>
+          <FaUsers size={25} />
+        </button>
+
+        {isOpen && (
+          <div className="slideBar">
+            <button className="closeButton" onClick={togglePanel}>
+              &times;
+            </button>
+
+            <div className="onlineContainer">
+              <div className="onlineDot" />
+              <p className="onlineTag">Online</p>
+            </div>
+
+            <div className="content">
+              <div className="userContainerWrapper">
+                <div className="userContainer">
+                  {users.map((user, index) => (
+                    <div className="user" key={index}>
+                      <p className="userName">{user}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="chatContainer">
+        <div className="messageBox" ref={messageBoxRef}>
+          {messages.map((msg, index) => (
+            <div className={`message ${msg.position}`} key={index}>
+              <p>{msg.sender}: {msg.content}</p>
+            </div>
+          ))}
+        </div>
+        <div className="inputContainer">
+        <div
+            className={`textField ${message ? 'hasValue' : ''}`}
+            contentEditable="true"
+            onInput={handleInputChange}
+            onKeyDown={handleInputChange}
+            placeholder="Message"
+          ></div>
+          <button className="sendButton" onClick={handleSend}>
+            <FaPaperPlane size={25} /></button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
